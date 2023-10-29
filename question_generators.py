77cee9ef4e032f66b70604ae7f6679c5d2efd62a
Henryjjjs <148510667+Henryjjjs@users.noreply.github.com>
2023-10-29 13:04:21 -0500

    change range of base

--- question_generators.py
+++ question_generators.py
@@ -95,8 +95,8 @@ class RepeatingSummation(QuestionGenerator):
 class BaseAddition(QuestionGenerator):
     def generate_value(self):
         base1 = random.randint(2, 9)
-        x = [str(random.randint(0, base1 - 1)) for _ in range(2)]
-        y = [str(random.randint(0, base1 - 1)) for _ in range(2)]
+        x = [str(random.randint(1, base1 - 1)) for _ in range(2)]
+        y = [str(random.randint(1, base1 - 1)) for _ in range(2)]
 
         return [base1, ''.join(x),  ''.join(y)]
 
